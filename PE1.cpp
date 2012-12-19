8e09bc67e863f597807d7adc08c7686b6234a9c4
Zobayer Hasan <zobayer1@gmail.com>
2012-12-20 03:14:14 +0600

    Problem 1

--- PE1.cpp
+++ PE1.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
 	int i, sum = 0;
-	for(i=1; i<1000; i++)
-		if(i % 3 == 0 || i % 5 == 0)
-			sum += i;
+	for(i=1; i<1000; i++) if(i%3==0 || i%5==0) sum += i;
 	printf("%d\n", sum);
 	return 0;
 }
